0a3a77d198fbb70e28f9f8ea9c6019cd835d09db
Jules Baud <baud.jules@gmail.com>
2015-05-13 20:18:00 +0200

    Push dernier version

---
+++ src/main/java/com/graphic/Eternity.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class Eternity
+{
+	public static void main(String[] args)
+	{
+		//Lancement de l'interface graphique
+		Gui interfaceGraphique = new Gui();
+		interfaceGraphique.setVisible(true);
+	}
+}
